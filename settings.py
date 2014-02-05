e4dffd60bb88a957000ad3d48620b26a96fe0be2
Ray Ch <i@iraycd.com>
2014-02-06 03:27:51 +0530

    Allow anonymous to access

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 ALLOW_ANONYMOUS_POSTS = False
-ALLOW_ANONYMOUS_ACCESS = False
+ALLOW_ANONYMOUS_ACCESS = True
 
 REPO_DIR = os.sep.join([os.path.dirname(os.path.abspath(__file__)), 'repositories'])
 
